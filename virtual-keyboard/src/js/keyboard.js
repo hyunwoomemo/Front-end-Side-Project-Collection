36e95bafc60dc5b2dcca6899e6baef445322206f
hyunwoomemo <archihw94@gmail.com>
2023-06-23 23:44:27 +0900

    key event & mouse event

--- virtual-keyboard/src/js/keyboard.js
+++ virtual-keyboard/src/js/keyboard.js
@@ -1,20 +1,104 @@
 export class Keyboard {
   #swichEl;
+  #fontSelectEl;
+  #containerEl;
+  #keyboardEl;
+  #inputGroupEl;
+  #inputEl;
+  #keyPress = false;
+  #mouseDown = false;
   constructor() {
     this.#assignElement();
     this.#addEvent();
   }
 
   #assignElement() {
-    this.#swichEl = document.querySelector("#switch");
+    this.#containerEl = document.querySelector("#container");
+    this.#swichEl = this.#containerEl.querySelector("#switch");
+    this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
+    this.#inputGroupEl = this.#containerEl.querySelector("#input-group");
+    this.#inputEl = this.#inputGroupEl.querySelector("#input");
   }
 
   #addEvent() {
-    this.#swichEl.addEventListener("change", (event) => {
-      document.documentElement.setAttribute(
-        "theme",
-        event.target.checked ? "dark-mode" : ""
-      );
-    });
+    this.#swichEl.addEventListener("change", this.#onChangeTheme);
+    this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+    document.addEventListener("keydown", this.#onKeyDown.bind(this));
+
+    document.addEventListener("keyup", this.#onKeyUp.bind(this));
+    this.#inputEl.addEventListener("input", this.#onInput);
+    this.#keyboardEl.addEventListener(
+      "mousedown",
+      this.#onMouseDown.bind(this)
+    );
+    document.addEventListener("mouseup", this.#onMouseUP.bind(this));
+  }
+
+  #onMouseDown(event) {
+    if (this.#keyPress) return;
+    this.#mouseDown = true;
+    event.target.closest("div.key")?.classList.add("active");
+  }
+  #onMouseUP(event) {
+    if (this.#keyPress) return;
+    this.#mouseDown = false;
+    const keyEl = event.target.closest("div.key");
+    const isActive = !!keyEl?.classList.contains("active");
+    const val = keyEl?.dataset.val;
+
+    if (isActive && !!val && val !== "Space" && val !== "Backspace") {
+      this.#inputEl.value += val;
+    }
+
+    if (isActive && val === "Space") {
+      this.#inputEl.value += " ";
+    }
+
+    if (isActive && val === "Backspace") {
+      this.#inputEl.value = this.#inputEl.value.slice(0, -1);
+    }
+
+    this.#keyboardEl.querySelector(".active")?.classList.remove("active");
+  }
+
+  #onChangeTheme(event) {
+    document.documentElement.setAttribute(
+      "theme",
+      event.target.checked ? "dark-mode" : ""
+    );
+  }
+
+  #onChangeFont(event) {
+    document.body.style.fontFamily = event.target.value;
+    console.log(event.target.value);
+  }
+
+  #onInput(event) {
+    event.target.value = event.target.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
+  }
+
+  #onKeyUp(event) {
+    if (this.#mouseDown) return;
+    this.#keyPress = false;
+    if (this.#keyboardEl.querySelector(`[data-code=${event.code}]`)) {
+      this.#keyboardEl
+        .querySelector(`[data-code=${event.code}]`)
+        .classList.remove("active");
+    }
+  }
+
+  #onKeyDown(event) {
+    if (this.#mouseDown) return;
+    this.#keyPress = true;
+    this.#inputGroupEl.classList.toggle(
+      "error",
+      /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key)
+    );
+    if (this.#keyboardEl.querySelector(`[data-code=${event.code}]`)) {
+      this.#keyboardEl
+        .querySelector(`[data-code=${event.code}]`)
+        .classList.add("active");
+    }
   }
 }
